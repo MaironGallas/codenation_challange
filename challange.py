49c595e64baace408394e3ea1477b84e5f0bb72c
Mairon <mairongallas@gmail.com>
2020-04-29 01:01:45 -0300

    Criado metodo request_challange

--- challange.py
+++ challange.py
@@ -1,3 +1,7 @@
+import requests
+import json
+
+
 def julio_cesar_decoder(number, code_text):
     char_decoded = []
     for letter in code_text:
@@ -12,3 +16,9 @@ def julio_cesar_decoder(number, code_text):
             ascii_number = char - number
         char_decoded.append(chr(ascii_number))
     return "".join(char_decoded)
+
+def request_challange(url):
+    data = requests.get(url)
+    data_dct = json.loads(data.content)
+    with open("answer.json", "w") as f:
+        json.dump(data_dct, f)
