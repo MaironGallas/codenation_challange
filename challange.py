427068efc5dcb772bbe258561b3868ceb3aba12f
Mairon <mairongallas@gmail.com>
2020-04-29 01:12:08 -0300

    Final Version!

--- challange.py
+++ challange.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import hashlib
+from decouple import config
 
 def julio_cesar_decoder(number, code_text):
     char_decoded = []
@@ -22,3 +23,37 @@ def request_challange(url):
     data_dct = json.loads(data.content)
     with open("answer.json", "w") as f:
         json.dump(data_dct, f)
+
+def read_properties(properties):
+    with open('answer.json', 'r') as arq:
+        data_readed = json.load(arq)
+    return data_readed[f'{properties}']
+
+def generate_cryptographic_summary(decode_text):
+    return hashlib.sha1(decode_text.encode()).hexdigest()
+
+def update_json(decode_text):
+    cryptographic_summary = generate_cryptographic_summary(decode_text)
+    with open('answer.json', 'r+') as f:
+        data = json.load(f)
+        data['decifrado'] = decode_text
+        data['resumo_criptografico'] = cryptographic_summary
+        f.seek(0)
+        json.dump(data, f)
+        f.truncate()
+
+def send_form(url):
+    answer = {'answer': open('answer.json', 'rb')}
+    submit = requests.post(url, files=answer)
+    print(submit.headers)
+
+
+if __name__ == '__main__':
+    URL_REQUEST = f"https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token={config('TOKEN')}"
+    URL_POST = f"https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token={config('TOKEN')}"
+
+    request_challange(URL_REQUEST)
+    numero_casas, cifrado = read_properties('numero_casas'), read_properties('cifrado')
+    texto_decodificado = julio_cesar_decoder(numero_casas, cifrado)
+    update_json(texto_decodificado)
+    send_form(URL_POST)
